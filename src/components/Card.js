3c48c8119f95c2a8ae8f2169443717121cf21fc8
Jacob Majcan <jacob.majcan@verosource.com>
2021-09-04 22:12:40 -0300

    reuploading trivia project

---
+++ src/components/Card.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react"
+import '../css/Card.css'
+
+class Card extends Component {
+  constructor(){
+    super()
+    this.state = {
+      isVisited: false
+    }
+  }
+  showModal = () => {
+    const { modalFn } = this.props
+    modalFn()
+    this.setState({
+      isVisited: true
+    })
+  }
+
+  render() {
+    const { category, value } = this.props
+    const { isVisited } = this.state
+
+    if (category) {
+      return <div className="card header">{category}</div>
+    } else {
+      return (
+        <div className={`card ${isVisited ? 'visited' : ''}`} onClick={this.showModal}>
+          ${value}
+        </div>
+      )
+    }
+  }
+}
+
+export default Card
---
+++ src/components/Category.js
@@ -0,0 +1,25 @@
+import React from "react"
+import Card from "./Card"
+import '../css/Category.css'
+
+const Category = props => {
+  const {questions, modalFn, name} = props
+
+  const cards = questions.map((q, index) => {
+    return (
+      <Card
+        key={index}
+        modalFn={() => modalFn(q)}
+        value={q.value}
+      />
+    )
+  })
+  return (
+    <div className="category">
+      <Card category={name} />
+      {cards}
+    </div>
+  )
+}
+
+export default Category
